750e04addca46227f50024a61d11aecaa90b91cd
Sebastean Gutierrez <tsakurai84@gmail.com>
2021-04-20 18:47:15 -0500

    inicializando el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gimnasio_pokemon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
