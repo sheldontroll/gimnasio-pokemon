0aa15aa946982d859cb615796e500a9f6d0d8605
Sebastean Gutierrez <tsakurai84@gmail.com>
2021-04-25 15:49:41 -0500

    AntesdeEntityFramework

--- Controllers/CentroPController.cs
+++ Controllers/CentroPController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using gimnasio_pokemon.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gimnasio_pokemon.Controllers
@@ -8,5 +10,24 @@ namespace gimnasio_pokemon.Controllers
             
             return View();
         }
+
+        
+
+        [HttpPost]
+
+        public IActionResult Nuevo(Centro c ) {
+            
+            if(ModelState.IsValid){
+                return RedirectToAction("Lista");
+
+            }
+            return View(c);
+        }
+
+        public IActionResult Lista(){
+            var centros = new List<Centro>();
+            return View(centros);
+        }
+
     }
 }
